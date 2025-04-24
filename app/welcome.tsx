7e2f27c804e2e93f0bde0f9851132682c360fd24
NukGiwi <nene.nanipas@gmail.com>
2025-04-24 22:54:17 +0700

    update

--- app/welcome.tsx
+++ app/welcome.tsx
@@ -4,11 +4,13 @@ import {icons} from "@/constants/icons";
 export default function Index() {
     return (
         <View className="flex w-full h-full p-[22px_15px] flex-col justify-center items-center bg-primary">
-            <View className="w-[98%] h-[98%] flex-shrink-0 rounded-[40px] bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]">
+            <View className="w-[98%] h-[98%] flex-shrink-0 rounded-[40px] bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] overflow-hidden">
                 <Image source={icons.welcome} className="w-[250px] mt-16 ml-8" />
                 <Text className="ml-8 mt-3 mr-7">ระบบจัดทำโครงการคาร์บอนเครดิตผ่านแอปพลิเคชั่นในมือถือ
                     มุ่งเน้นการให้บริการที่เข้าถึงและใช้งานง่ายต่อเกษตรกร
                     ลดระยะเวลาที่จะการเริ่มดำเนินผลและเก็บข้อมูล</Text>
+                <View className="flex-1" />
+                <Image source={icons.green} className="w-full h-[120px]" resizeMode="cover"/>
             </View>
         </View>
     )
--- constants/icons.ts
+++ constants/icons.ts
@@ -3,6 +3,8 @@ import logo from "@/assets/icons/logo.png";
 import logo_frame from "@/assets/icons/logo_frame.png";
 import logo_text from "@/assets/icons/logo_text.png";
 import welcome from "@/assets/icons/welcome.png";
+import green from "@/assets/icons/green.png";
+
 
 export const icons = {
     go_button,
@@ -10,4 +12,5 @@ export const icons = {
     logo_frame,
     logo_text,
     welcome,
+    green
 }
